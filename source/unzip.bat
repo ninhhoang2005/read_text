2ed988bb9c36f1bb251689080a42d0900dd51a9c
ninhhoang2005 <nguyenninhhoang2005@gmail.com>
2026-01-27 04:27:08 +0700

    fix bug
    fix run file.exe

--- source/unzip.bat
+++ source/unzip.bat
@@ -2,5 +2,5 @@
 title "Updating is in progress, please do not close this window"
 powershell -command "Expand-Archive -Path 'read_text.zip' -DestinationPath '.' -Force"
 del read_text.zip
-start "" read_text.exe
+start "" ReadText.exe
 exit

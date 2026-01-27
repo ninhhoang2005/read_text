4c77112ad1e395447a300a276b0a3e04a6cbeac2
ninhhoang2005 <nguyenninhhoang2005@gmail.com>
2026-01-27 20:02:29 +0700

    FIX BUG
    FIX extracter

--- source/unzip.bat
+++ source/unzip.bat
@@ -1,11 +1,6 @@
 @echo off
-
-title Updating is in progress, please do not close this window
-
+title "Updating is in progress, please do not close this window"
 powershell -command "Expand-Archive -Path 'read_text.zip' -DestinationPath '.' -Force"
-
-del "read_text.zip"
-
-start "" "ReadText.exe"
-
+del read_text.zip
+start "" ReadText.exe
 exit

1e4fbcea05a0c99d5292c03558eff94379ac9cc1
ninhhoang2005 <nguyenninhhoang2005@gmail.com>
2026-01-27 19:53:26 +0700

    fix bug
    fix extracter file.zip

--- source/unzip.bat
+++ source/unzip.bat
@@ -1,17 +1,10 @@
 @echo off
-cd /d "%~dp0"
 
 title Updating is in progress, please do not close this window
 
-if not exist "read_text.zip" (
-    echo Error: File read_text.zip not found!
-    pause
-    exit
-)
-
 powershell -command "Expand-Archive -Path 'read_text.zip' -DestinationPath '.' -Force"
 
-if exist "read_text.zip" del "read_text.zip"
+del "read_text.zip"
 
 start "" "ReadText.exe"
 
